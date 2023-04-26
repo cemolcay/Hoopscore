c0e945d449ca690eb62b072f39eefe6278ec3780
Cem Olcay <ccemolcay@gmail.com>
2023-04-26 13:00:54 +0300

    Initial Commit

---
+++ Hoopscore/HoopscoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoopscoreApp.swift
+//  Hoopscore
+//
+//  Created by Cem Olcay on 4/26/23.
+//
+
+import SwiftUI
+
+@main
+struct HoopscoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
